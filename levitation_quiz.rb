cd681d1904ce64c0188887a728438a6c7b39a1a4
Arslan Ali <ali.arslan319@gmail.com>
2019-03-23 06:43:58 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,13 @@
 
 def levitation_quiz
-	#your code here
+
+loop do
+        puts "What is the spell that enacts levitation?"
+        answer = gets.chomp
+        break if answer == "Wingardium Leviosa"
+    end
+    puts "Okay, okay, jeez. I'll stop. Sorry."
 end
 
-
+#your code here
+end
